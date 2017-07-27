dd3f9c706982ed5c80b22d41345b5fde07dafcee
Chris-Maina <krismaish34@gmail.com>
2017-07-28 00:55:31 +0300

    [UPDATE]Add bucketlist route to handle creation of buckets

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
+from flask import request, jsonify, abort
 
 # local import
 from instance.config import app_config
@@ -14,10 +15,45 @@ def create_app(config_name):
      loads it with configs using app.config,
      connects it with DB,
      returns it  """
+    from app.models import Bucketlist
+
+
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
+    @app.route('/bucketlist/', methods=['POST', 'GET'])
+    def bucketlists():
+        if request.method == "POST":
+            name = str(request.data.get('name', ''))
+            if name:
+                bucketlist = Bucketlist(name=name)
+                bucketlist.save()
+                response = jsonify({
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                })
+                response.status_code = 201
+                return response
+        else:
+            # GET
+            bucketlists_all = Bucketlist.get_all()
+            results = []
+            for bucketlist in bucketlists_all:
+                obj = {
+                    'id': bucketlist.id,
+                    'name': bucketlist.name,
+                    'date_created': bucketlist.date_created,
+                    'date_modified': bucketlist.date_modified
+                }
+                results.append(obj)
+
+            response = jsonify(results)
+            response.status_code = 200
+            return response
 
     return app
