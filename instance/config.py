58ed3fbd14b1fab02d829355389a62a13dbabfae
Chris-Maina <krismaish34@gmail.com>
2017-08-05 11:17:18 +0300

    [Update]config.py to add database URI

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,6 @@
 # /instance/config.py
 
+
 class Config(object):
     """Parent config class."""
     DEBUG = False
@@ -7,9 +8,11 @@ class Config(object):
     SECRET = "hardworkpayseverytime"
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:chris@localhost:5432/flask_api"
 
+
 class DevelopmentConfig(Config):
     """Configs for development"""
     DEBUG = True
+
 
 class TestingConfig(Config):
     """Configs for testing, with a separate test db"""
@@ -17,14 +20,18 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:chris@localhost:5432/test_db"
     DEBUG = True
 
+
 class StagingConfig(Config):
     """Configs for staging"""
     DEBUG = True
+
 
 class ProductionConfig(Config):
     """Configs for production"""
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = "postgres://yroreitiyrpffu:a31393a9e7006ea18452376c29c599d5051e6f38fe56d3cc59c81ef262dba4b5@ec2-54-227-252-202.compute-1.amazonaws.com:5432/dbpp9omfchlvo6"
+
 
 app_config = {
     'development': DevelopmentConfig,
